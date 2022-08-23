1f114f515d172606a82535ef2b800d5a684e624a
ElijahBcodes <eli1inthesnow@gmail.com>
2022-08-23 13:47:28 -0700

    intro to Ruby

---
+++ intro-elijah-hieu.rb
@@ -0,0 +1,62 @@
+# Challenges
+# ➕ Arithmetic Challenges
+
+# Add, subtract, multiply and divide some numbers and floats.
+1+1 = 2
+1-1 = 0 
+1*2 = 2
+1.5*2 =3
+
+# Find the remainder of dividing two numbers using the modulo operator (%).
+3 % 2 = 1
+# Divide a number by 0.
+7 / 0 = zero division error 
+# Divide 0 by 0.
+zero division error
+# = Variables Challenges
+# Set a variable called my_favorite_number equal to your favorite number
+my_fav_num = 21
+# Calculate your favorite number divided by 2
+21 / 2 = 10 
+# Set another variable called someones_favorite equal to 13
+someones_favorite = 13
+# Change the value of someones_favorite to 7
+someones_favorite = 7 
+# Subtract my_favorite_number from someones_favorite
+my_favorite_number- someones_favorite = -14
+# Change the value of my favorite number to be 26 times its current value
+# 🧵 Strings Challenges
+# Create a variable and return it in a sentence string interpolation
+my_variable = 'Hello'
+"#{my_variable}"
+# Create a variable that contains a string and test some of the Ruby string methods
+my_variable.upcase
+# .upcase
+my_variable.reverse
+# .reverse
+my_variable.include?
+# .include?
+my_variable.capitalize
+# .capitalize
+my_variable.delete
+# .delete
+my_variable.index
+# .index
+my_variable.swapcase
+# .swapcase
+# [] Arrays Challenges
+# Create an array that contains the name of at least five TV shows
+tv_shows = ['House', 'Matlock', 'Spongebob', 'Simpsons', 'Friends']
+# Find the length of the array
+tv_shows.length
+# Return the first item in the array
+tv_shows[0]
+# Return the fourth item in the array
+tv_shows[3]
+# Permanently reverse the order of the array
+tv_shows.reverse
+
+# Create a new empty array for your top favorite TV shows
+tv = []
+# From the full TV show array, add your top two favorite shows to the empty array
+tv_shows << tv
