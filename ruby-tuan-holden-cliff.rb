c4de8cde95114087b35857a083bbc80480256c2f
TuanLeVietz <tuanz_z@yahoo.com>
2022-08-23 13:13:16 -0700

    first 5

---
+++ ruby-tuan-holden-cliff.rb
@@ -0,0 +1,16 @@
+# Add, subtract, multiply and divide some numbers and floats.
+p 10 - 2
+p 5 + 5
+p 6 * 2
+p 21 / 3
+p 5.6 - 4.4
+
+# Find the remainder of dividing two numbers using the modulo operator (%).
+p 9 % 3
+p 9 % 2
+p 9 % 5
+# Divide a number by 0.
+p 21 / 0
+p 10 / 0
+# Divide 0 by 0.
+p 0.0 / 0
