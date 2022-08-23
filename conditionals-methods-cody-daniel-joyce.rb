0c60d6aad2d77744aa31875abe01eafecdd0d668
dnlclmnt <dnlclmnt@gmail.com>
2022-08-23 15:55:43 -0400

    completed first set of challenges

---
+++ conditionals-methods-cody-daniel-joyce.rb
@@ -0,0 +1,56 @@
+# ❤️ Challenges
+# Create a method called sum_these_numbers which takes two integers as an argument and prints their sum to the screen.
+
+def sum_these_numbers(num_one, num_two)
+    p num_one + num_two
+end
+
+sum_these_numbers(63, 47)
+
+# Create a method called is_even, which takes a single integer, and which then returns true if the number is even, and false otherwise.
+
+def is_even int
+    p int.even?
+end
+
+is_even 4
+is_even 9
+
+# Create a method that takes a number as an argument and prints "Valid" if the number is between 1 and 10 (inclusive) and "Invalid" otherwise.
+
+def is_valid num
+    p num > 0 && num < 11 ? "#{num} is Valid" : "#{num} is Invalid"
+end
+
+is_valid 8
+is_valid 15
+# Create a method that takes in a string and determines if the string is a palindrome.
+
+def is_pal str
+    if str == str.reverse
+        p "#{str} is a palindrome"
+    else
+        p "#{str} is NOT a palindrome"
+    end
+end
+
+is_pal 'racecar'
+is_pal 'level'
+is_pal 'macaroni'
+is_pal 'nurses run'
+# ✂️ Challenge: Rock, Paper, Scissors
+# As the first user, I can see a prompt in the terminal to enter my name.
+# As the second user, I can see a prompt in the terminal to enter my name.
+# As the first user, I can see a prompt in the terminal asking me to type either "rock", "paper", or "scissors".
+# As the second user, I can see a prompt in the terminal asking me to type either "rock", "paper", or "scissors".
+# As a user, I can see a message in the terminal depicting which user won the round.
+# As a user, I can see a message in the terminal noting if there was a tie.
+# ✅ Challenge: Password Checker
+# As a developer, you have been tasked with creating the user registration for a secure web site. To test your code, enter the user ID and password in the terminal to find out if they are acceptable. The user ID and password must adhere to the to following criteria:
+
+# User ID and password cannot be the same.
+# User ID and password must be at least six characters long.
+# Password must contain at least one of: !#$
+# User ID cannot contain the following characters: !#$ or spaces
+# Password cannot be the word "password".
+# User password must contain at least one number.
