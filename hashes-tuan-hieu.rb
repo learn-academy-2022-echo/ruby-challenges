6699088caaee96b5969fe1588422f0053e12e75f
hieuhuule <hieuhuule@gmail.com>
2022-08-24 18:24:09 -0500

    completed all the hash challenges

--- hashes-tuan-hieu.rb
+++ hashes-tuan-hieu.rb
@@ -11,26 +11,36 @@ my_phone[:facebook] = 'social media app'
 # puts my_phone
 
 # As a developer, I can return a value from my_phone by passing in the name of a key.
-puts my_phone[:tinder]
+
+# puts my_phone[:tinder]
+
 # As a developer, I can update two keys in my_phone.
+
 my_phone[:hearthstone] = my_phone.delete(:candy_crush)
 my_phone[:twitter] = my_phone.delete(:facebook)
 # p my_phone
+
 # As a developer, I can update two values in my_phone.
+
 my_phone[:tinder] = 'dating app for sad hookups'
 my_phone[:spotify] = 'music app that no one uses anymore'
+
 # As a developer, I can delete two key:value pairs from my_phone.
+
 my_phone.delete(:tinder)
 my_phone.delete(:twitter)
 # p my_phone
+
 # As a developer, I can use an enumerable method to return information about all of my_phone's applications.
+
 # my_phone.map do |key, value|
 #    p "The #{key} app is a #{value}" 
-
 # end
 
 # 🏔 Stretch Goals
+
 # As a developer, I can create a custom method that takes in my_phone and returns an array with the app name capitalized and information about each phone app.
+
 def capital_name_and_description obj
     new_arr = []
     obj.each do |key, value|
@@ -38,5 +48,18 @@ def capital_name_and_description obj
     end
     p new_arr
 end
-capital_name_and_description my_phone
+# capital_name_and_description my_phone
+
 # As a developer, I can create a custom method that takes in my_phone and returns an array with a sentence about the name of each app.
+
+
+# returns an array 
+
+def custom_name my_phone
+    new_arr = []
+    my_phone.map do |key, value|
+        new_arr << "#{key} is an application for your phone"
+    end
+    p new_arr
+end
+custom_name my_phone
