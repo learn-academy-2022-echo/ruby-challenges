02f376b458600f960a7b8d4e42632131fab2d3ba
sanjuel <samueljscott101@gmail.com>
2022-08-26 11:53:40 -0700

    task done

--- classes-sam-brook.rb
+++ classes-sam-brook.rb
@@ -3,8 +3,8 @@
 # As a developer, I can create three instances (objects) of class Task. (e.g laundry = Task.new)✅
 # As a developer, I can initialize each instance of class Task with a title.✅
 # As a developer, I can see the title of each instance of class Task.✅
-# As a developer, I can initialize each instance of class Task with a status that has a default value of 'incomplete'.
-# As a developer, I can update the status of each instance of class Task when the task has been completed.
+# As a developer, I can initialize each instance of class Task with a status that has a default value of 'incomplete'.✅
+# As a developer, I can update the status of each instance of class Task when the task has been completed. ✅
 
 class Task 
  def set_name name
@@ -27,4 +27,18 @@ p laundry.get_name
 p dishes.get_name
 p cooking.get_name
 
+class Task
+    attr_accessor :status
 
+    def initialize(status)
+        @status = status
+    end
+end
+
+laundry = Task.new("Complete")
+dishes = Task.new("Complete")
+cooking = Task.new("Complete")
+
+p laundry.status
+p dishes.status
+p cooking.status
