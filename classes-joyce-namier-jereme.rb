39a022a69dde17c3a13806a2cc2b9bd17008caa5
jmagistrado <joycemagistrado@gmail.com>
2022-08-26 11:58:43 -0700

    here is our answers

--- classes-joyce-namier-jereme.rb
+++ classes-joyce-namier-jereme.rb
@@ -3,12 +3,12 @@
 # For the following Task challenge, use initialize and getter methods in your class
 # As a developer, I can create a class called Task.
 
-class Task 
-    def set_task value
-        @task_name = value
+# class Task 
+#     def set_task value
+#         @task_name = value
 
-    end
-end 
+#     end
+# end 
 
 # As a developer, I can create three instances (objects) of class Task. (e.g laundry = Task.new)
 
@@ -18,25 +18,51 @@ end
 
 # As a developer, I can initialize each instance of class Task with a title.
 
-p task_one = Task.new
-p task_two = Task.new
-p task_three = Task.new 
+# p task_one = Task.new
+# p task_two = Task.new
+# p task_three = Task.new 
 
 
 # As a developer, I can see the title of each instance of class Task.
 
-p task_one.set_task('laundry')
-p task_two.set_task('dishes')
-p task_three.set_task('vacuum')
+# p task_one.set_task('laundry')
+# p task_two.set_task('dishes')
+# p task_three.set_task('vacuum')
 
 
 # As a developer, I can initialize each instance of class Task with a status that has a default value of 'incomplete'.
 
-class TaskTwo
-    def initialize ()
+# class TaskTwo
+#     def initialize 
+#         @laundry = 'incomplete'
+#         @dishes = 'incomplete'
+#         @vacuum = 'incomplete'
+#     end
+# end
 
-        
+# p task_name = TaskTwo.new
+#output: #<TaskTwo:0x00007ff2588a0ff0 @laundry="incomplete", @dishes="incomplete", @vacuum="incomplete">
+
+# As a developer, I can update the status of each instance of class Task when the task has been completed.
+
+class TaskTwo
+    attr_accessor :laundry, :dishes, :vacuum
+    def initialize (laundry, dishes, vacuum)
+        @laundry = laundry
+        @dishes = dishes
+        @vacuum = vacuum
     end
 end
 
-# As a developer, I can update the status of each instance of class Task when the task has been completed.
+
+namier = TaskTwo.new('incomplete', 'incomplete', 'incomplete')
+# p namier.laundry
+
+namier.vacuum = 'complete'
+p namier.vacuum
+namier.dishes = 'complete'
+p namier.dishes
+namier.laundry = 'complete'
+p namier.laundry
+
+
