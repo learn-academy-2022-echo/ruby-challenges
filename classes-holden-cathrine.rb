d1f9e191a4f1fa68428702164e5ecafe49aea0f1
holdenprine <holdenprine404@gmail.com>
2022-08-26 11:32:07 -0700

    All challenges complete

--- classes-holden-cathrine.rb
+++ classes-holden-cathrine.rb
@@ -34,3 +34,30 @@ p sweep
 # As a developer, I can print the value of each individual color.
 # As a developer, I can create a method called all_colors that when called will print a sentence telling me the three colors of a given palette.
 # As a developer, I can change one or more colors of a given palette.
+
+class ColorPalette
+    attr_accessor :color_one, :color_two, :color_three, :name
+    
+    def initialize color_one, color_two, color_three, name
+        @color_one = color_one
+        @color_two = color_two
+        @color_three = color_three
+        @name = name
+    end
+
+    def all_colors 
+        "the different shades of #{name} are #{color_one}, #{color_two}, #{color_three}."
+    end
+end
+
+blue = ColorPalette.new('Teal', 'Navy', 'Cobalt', 'blue')
+orange = ColorPalette.new('Melon', 'Atomic Tangerine', 'Xanthous', 'orange')
+white = ColorPalette.new('Egg Shell', 'Pearl', 'Offwhite', 'white')
+
+blue.color_one = 'Turquoise'
+orange.color_two = 'Pumpkin'
+white.color_three = 'Ivory'
+
+p blue.all_colors
+p orange.all_colors
+p white.all_colors
