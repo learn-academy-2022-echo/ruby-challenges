98e40cfd95a96874434dc94062854fe5cbd980b3
JPD99 <jeremedoan@gmail.com>
2022-08-29 16:34:32 -0700

    Passing Off the Task Challenge

---
+++ rspec-clifford-rashad-jereme/task.rb
@@ -0,0 +1,37 @@
+# User Stories
+# Story: As a developer, I can create a Task.
+
+# class Task
+# end
+
+    #Finished in 0.00743 seconds (files took 0.17083 seconds to load)
+    # 1 example, 0 failures
+
+# Story: As a developer, I can give a Task a title and retrieve it.
+
+class Task 
+    attr_accessor :title
+    def initialize 
+        @title = title
+    end
+
+    def title
+        @title = title
+    end
+end 
+
+# Story: As a developer, I can give a Task a description and retrieve it.
+
+# Story: As a developer, I can mark a Task done. Tasks should be initialized as 'in progress'.
+
+# Story: As a developer, when I print a Task that is done, its status is shown.
+
+# Story: As a developer, I can add all of my Tasks to a TaskList.
+
+# Story: As a developer with a TaskList, I can print the completed items.
+
+# Story: As a developer with a TaskList, I can print the incomplete items.
+
+
+
+
