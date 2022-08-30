45c3dcb54fb488650facf4f1ec7727b63e6b6001
CathrineC <cjsr.carreon@gmail.com>
2022-08-29 20:29:39 -0700

    still in progress

--- rspec-elijah-cathrine/task.rb
+++ rspec-elijah-cathrine/task.rb
@@ -1,5 +1,20 @@
-
-
 class Task
-
+    attr_accessor :title, :description, :done, :newtasklist
+    def initialize
+        @title = title
+        @description = description
+        @done = false
+        @newtasklist = []
+    end
+    def is_completed
+        if @done == true
+        p 'Done!'
+        else
+        p 'In progress!'
+        end
+    end
+    def add_new_task list
+        @newtasklist << list       
+    end
 end
+
